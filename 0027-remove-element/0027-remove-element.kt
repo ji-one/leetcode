dd0ad3dc0899fc45fb00196818060dbf3eb6f1a6
ji-one <rlawldnjs8@gmail.com>
2023-08-10 23:57:45 +0900

    Time: 222 ms (5.03%), Space: 37.6 MB (6.39%) - LeetHub

--- 0027-remove-element/0027-remove-element.kt
+++ 0027-remove-element/0027-remove-element.kt
@@ -1,10 +1,9 @@
 class Solution {
     fun removeElement(nums: IntArray, `val`: Int): Int      {
-        nums.indices.filter { nums[it] === `val` }
-        .forEach { nums[it] = -1 }
+        nums.indices.filter { nums[it] == `val` }
+            .forEach { nums[it] = -1 }
         nums.sortDescending()
-        
-        return nums.count { it !== -1 }
-        
+
+        return nums.count { it != -1 }
     }
 }
