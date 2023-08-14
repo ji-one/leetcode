dd3d96e51a80bf9c7b2a59db36453eb5a4249737
ji-one <rlawldnjs8@gmail.com>
2023-08-15 00:24:23 +0900

    Time: 209 ms, Memory: 36.4 MB - LeetHub

--- valid-anagram/valid-anagram.kt
+++ valid-anagram/valid-anagram.kt
@@ -2,7 +2,7 @@ class Solution {
     fun isAnagram(s: String, t: String): Boolean {
         val charCountMap = mutableMapOf<Char, Int>()
 
-        if(s.length != t.length) return false
+        if (s.length != t.length) return false
 
         for (i in s.indices) {
             charCountMap[s[i]] = charCountMap.getOrDefault(s[i], 0) + 1
