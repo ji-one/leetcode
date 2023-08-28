79dbd0f08f4b4eae85664ab9260ac485f9c3d596
ji-one <rlawldnjs8@gmail.com>
2023-08-28 23:30:08 +0900

    Time: 157 ms, Memory: 35.3 MB - LeetHub

--- plus-one/plus-one.kt
+++ plus-one/plus-one.kt
@@ -1,23 +1,13 @@
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
+
         val digitsList = ArrayList<Int>(digits.size)
         digits.forEach { digitsList.add(it) }
 
-        var up = 0
-        if (digitsList.last() + 1 == 10) {
-            up = 1
-            digitsList[digitsList.size - 1] = 0
-            if (digits.size == 1) {
-                digitsList.add(0, 1)
-            }
-        } else {
-            digitsList[digitsList.size - 1] += 1
-        }
-
-        for (i in digits.size - 2 downTo 0 step 1) {
+        var up = 1
+        for (i in digits.size - 1 downTo 0 step 1) {
             if (digitsList[i] + up == 10) {
-                val tmp = (digitsList[i] + up)
-                digitsList[i] = tmp % 10
+                digitsList[i] = 0
                 up = 1
             } else {
                 digitsList[i] += up
